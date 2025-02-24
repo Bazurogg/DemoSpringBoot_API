9b97110813cf4df2d124b3c5c696e529ae6f375d
Bazurogg <bazurogg@gmail.com>
2025-02-24 11:33:02 +0100

    test workflow

--- src/test/java/fr/afpa/pompey/cda/demospringboot_api/PersonServiceTest.java
+++ src/test/java/fr/afpa/pompey/cda/demospringboot_api/PersonServiceTest.java
@@ -10,6 +10,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Optional;
@@ -17,6 +18,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+@ActiveProfiles("test")
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @ExtendWith(MockitoExtension.class)
